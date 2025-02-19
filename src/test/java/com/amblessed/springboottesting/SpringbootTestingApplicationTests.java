43176667761051debb7d092d011db861897843fc
Okechukwu Bright Onwumere <brighthamza9@yahoo.com>
2025-02-19 06:39:06 +0100

    Initial Commit

---
+++ src/test/java/com/amblessed/springboottesting/SpringbootTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amblessed.springboottesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTestingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
